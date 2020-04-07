d0e796f9deca80456d8295d092de593f670c8c32
Everett Yeaw <evyeaw@gmail.com>
2020-04-07 15:43:20 -0400

    added to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,24 +7,15 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  config.assets.js_compressor = Uglifier.new(harmony: true)
 
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  config.assets.compile = true
+  config.assets.compile = false
 
-  config.active_storage.service = :local
-
-  config.log_level = :debug
-
-  config.log_tags = [ :request_id ]
-
-  config.action_mailer.perform_caching = false
-
-  config.i18n.fallbacks = true
-
-  config.active_support.deprecation = :notify
-
-  config.log_formatter = ::Logger::Formatter.new
+  config.assets.digest = true
 
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
